3de4c27bc8eca6485495e35f45815f3d8d91c48b
JavaKaran <sharmakaran12092001@gmail.com>
2021-06-18 16:04:11 +0530

    changes committed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,8 +5,8 @@ module.exports = function(deployer) {
     // Token price is 0.001 Ether
     var tokenPrice = 1000000000000000;
     return deployer.deploy(kstSale, kst.address, tokenPrice);
-    }).then(function() {
-      var tokensAvailable = 750000;
-      kst.deployed().then(function(instance) { instance.transfer(kstSale.address, tokensAvailable, { from: "0x34892DC10c9dF4Efa352E2d4Fde9fA93548bC9Ea" }); })
-  });
+  }).then(function() {
+    var tokensAvailable = 750000;
+    kst.deployed().then(function(instance) { instance.transfer(kstSale.address, tokensAvailable, { from: "0x34892DC10c9dF4Efa352E2d4Fde9fA93548bC9Ea" }); })
+    });
 };
